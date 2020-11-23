a7118ac67219457fa7b4d3ff66c40f0da9b9185c
Lisciowsky <szumlas.kuba@gmail.com>
2020-11-23 19:27:02 +0100

    update, neural net

--- sentdex_series.py
+++ sentdex_series.py
@@ -290,43 +290,144 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm
-class DogsVsCats:
-    IMGSIZE = 50
+REBUILD_DATA = True
+os.chdir('/home/kuba/PetImages')
+
+class DogsVSCats():
     cat_count = 0
     dog_count = 0
-    CATS = 'PetImages/Cat'
-    DOGS = 'PetImages/Dog'
-    training_dataset = []
-    labels = {CATS : 0, DOGS : 1}
-    exc = []
     
-    def train_data(self):
-        for label in self.label:
-            for file in tqdm(os.listdir(label)):
-                if 'jpg' in file:
+    IMG_SIZE = 50
+    CATS = "/home/kuba/PetImages/Cat"
+    DOGS = "/home/kuba/PetImages/Dog"
+    TESTING = "PetImages/Testing"
+    LABELS = {CATS: 0, DOGS: 1}
+    training_data = []
+
+    def make_training_data(self):
+        for label in self.LABELS:
+            for f in tqdm(os.listdir(label)):
+                if "jpg" in f:
                     try:
-                        path = os.path.join(label, file)
+                        path = os.path.join(label, f)
                         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-                        img = cv2.resize(img, (self.IMGSIZE * self.IMGSIZE))
-                        self.training_dataset.append(np.array(img), np.eye(2)[self.labels][label])
+                        img = cv2.resize(img, (self.IMG_SIZE, self.IMG_SIZE))
+                        self.training_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])  # do something like print(np.eye(2)[1]), just makes one_hot 
                         if label == self.CATS:
                             self.cat_count += 1
                         elif label == self.DOGS:
                             self.dog_count += 1
                             
                     except Exception as e:
-                        self.exc += 1
                         pass
-    np.random.shuffle(self.training_dataset)
-    np.save("training_dataset.npy", self.training_dataset)
-    print('Cats', self.cat_count)
-    print('Dogs', self.dog_count)
+                        #print(label, f, str(e))
+                        
+        np.random.shuffle(self.training_data)
+        np.save('training_data.npy', self.training_data)
     
-                        
+        print('Dogs', DogsVSCats.dog_count)
+        print('Cats', DogsVSCats.cat_count)
+
 if REBUILD_DATA:
-    dogvcats = DogsVsCats()
-    dogvcats.train_data()                   
-                        
+    dogsvcats = DogsVSCats()
+    dogsvcats.make_training_data()
+  
+# =============================================================================
+# conv neur
+# =============================================================================
+  
+    
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__() # just run the init of parent class (nn.Module)
+        self.conv1 = nn.Conv2d(1, 32, 5) # input is 1 image, 32 output channels, 5x5 kernel / window
+        self.conv2 = nn.Conv2d(32, 64, 5) # input is 32, bc the first layer output 32. Then we say the output will be 64 channels, 5x5 kernel / window
+        self.conv3 = nn.Conv2d(64, 128, 5)
+
+        x = torch.randn(50,50).view(-1,1,50,50)
+        self._to_linear = None
+        self.convs(x)
+
+        self.fc1 = nn.Linear(self._to_linear, 512) #flattening.
+        self.fc2 = nn.Linear(512, 2) # 512 in, 2 out bc we're doing 2 classes (dog vs cat).
+
+    def convs(self, x):
+        # max pooling over 2x2
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
+
+        if self._to_linear is None:
+            self._to_linear = x[0].shape[0]*x[0].shape[1]*x[0].shape[2]
+        return x
+
+    def forward(self, x):
+        x = self.convs(x)
+        x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before 
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x) # bc this is our output layer. No activation here.
+        return F.softmax(x, dim=1)
+
+
+net = Net()
+print(net)    
+
+import torch.optim as optim
+
+optimizer = optim.Adam(net.parameters(), lr = 0.001)
+loss_function = nn.MSELoss()
+
+X = torch.Tensor([i[0] for i in training_data]).view(-1,1,50,50)
+X = X/255.0
+y = torch.Tensor([i[1] for i in training_data])
+
+val_pct = 0.1
+val_size = int(len(X)*val_pct)
+
+train_X = X[:-val_size]
+train_y = y[:-val_size]
+test_X = X[-val_size:]
+test_y = y[-val_size:]
+
+BATCH_SIZE = 100
+EPOCH = 1
+
+#train
+for epoch in range(EPOCH):
+    for i in tqdm(range(0,len(train_X), BATCH_SIZE)):
+        batch_X = train_X[i:i+BATCH_SIZE]
+        batch_y = train_y[i:i+BATCH_SIZE]
+        net.zero_grad()
+        outputs = net(batch_X)
+        loss = loss_function(outputs, batch_y)
+        loss.backward()
+        optimizer.step()
+        
+print(loss)
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for i in tqdm(range(len(test_X))):
+        real_class = torch.argmax(test_y[i])
+        net_out = net(test_X[i].view(-1,1,50,50))[0]
+        predicted_class = torch.argmax(net_out)
+        
+        if predicted_class == real_class:
+            correct += 1
+        total += 1
+    
+print(f"'Accuracy': {round((correct/total),3)}")
+
+   
+
+
+
 
 
 
