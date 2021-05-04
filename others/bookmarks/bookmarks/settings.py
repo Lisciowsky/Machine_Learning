16653d02ec69800a39a9e53c992f9ac4eaa02576
Lisciowsky <szumlas.kuba@gmail.com>
2021-05-04 21:50:36 +0200

    addded user  handling solutions "logging, reseting password etc"

--- others/bookmarks/bookmarks/settings.py
+++ others/bookmarks/bookmarks/settings.py
@@ -16,6 +16,8 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
