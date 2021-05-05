58d135347c54bb1c20c056f4a3f410bcdef3c34a
Lisciowsky <szumlas.kuba@gmail.com>
2021-05-05 23:49:42 +0200

    added admin profiles

--- others/bookmarks/bookmarks/settings.py
+++ others/bookmarks/bookmarks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
@@ -125,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_URL = '/media/'
+#MEDIA_ROOT is the local file where the uploaded files reside.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
